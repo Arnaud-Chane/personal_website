b781ff777b8bcca388793df83cc29a785c65251e
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-08 15:56:29 +0100

    Update about me content and mt to about me title

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -53,13 +53,11 @@ const AboutSection = () => {
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={500} height={500} alt="about me"/>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <h2 className="text-4xl font-bold text-white mb-4 mt-7">About Me</h2>
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
+            working with React, Typescript, TailwindCSS, C#, .NET, Microsoft SQL Server and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
