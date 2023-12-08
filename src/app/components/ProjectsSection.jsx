58d32f06f024cc992edae96d46599b7f8ce9fa53
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-08 16:13:57 +0100

    Condition to render projects if > 0

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -4,62 +4,64 @@ import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
-const projectsData = [
-  {
-    id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 2,
-    title: "Potography Portfolio Website",
-    description: "Project 2 description",
-    image: "/images/projects/2.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
-    image: "/images/projects/3.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-];
+const projectsData = [];
+
+// const projectsData = [
+//   {
+//     id: 1,
+//     title: "React Portfolio Website",
+//     description: "Project 1 description",
+//     image: "/images/projects/1.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 2,
+//     title: "Potography Portfolio Website",
+//     description: "Project 2 description",
+//     image: "/images/projects/2.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 3,
+//     title: "E-commerce Application",
+//     description: "Project 3 description",
+//     image: "/images/projects/3.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 4,
+//     title: "Food Ordering Application",
+//     description: "Project 4 description",
+//     image: "/images/projects/4.png",
+//     tag: ["All", "Mobile"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 5,
+//     title: "React Firebase Template",
+//     description: "Authentication and CRUD operations",
+//     image: "/images/projects/5.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+//   {
+//     id: 6,
+//     title: "Full-stack Roadmap",
+//     description: "Project 5 description",
+//     image: "/images/projects/6.png",
+//     tag: ["All", "Web"],
+//     gitUrl: "/",
+//     previewUrl: "/",
+//   },
+// ];
 
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
@@ -101,26 +103,33 @@ const ProjectsSection = () => {
           isSelected={tag === "Mobile"}
         />
       </div>
-      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project, index) => (
-          <motion.li
-            key={index}
-            variants={cardVariants}
-            initial="initial"
-            animate={isInView ? "animate" : "initial"}
-            transition={{ duration: 0.3, delay: index * 0.4 }}
-          >
-            <ProjectCard
-              key={project.id}
-              title={project.title}
-              description={project.description}
-              imgUrl={project.image}
-              gitUrl={project.gitUrl}
-              previewUrl={project.previewUrl}
-            />
-          </motion.li>
-        ))}
-      </ul>
+
+      {filteredProjects.length == 0 ? (
+        <div className="flex flex-row justify-center">
+          <div className="text-base lg:text-lg mt-20">Coming soon !</div>
+        </div>
+      ) : (
+        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+          {filteredProjects.map((project, index) => (
+            <motion.li
+              key={index}
+              variants={cardVariants}
+              initial="initial"
+              animate={isInView ? "animate" : "initial"}
+              transition={{ duration: 0.3, delay: index * 0.4 }}
+            >
+              <ProjectCard
+                key={project.id}
+                title={project.title}
+                description={project.description}
+                imgUrl={project.image}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
+              />
+            </motion.li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
