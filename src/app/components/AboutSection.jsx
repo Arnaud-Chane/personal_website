80118c4cbe67b638ba3088ec7ffb573d49cb93a9
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-06 15:56:08 +0100

    Update CV & Update Front-Back stacks

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,9 +9,10 @@ const TAB_DATA = [
     id: "frontend",
     content: (
       <ul className="list-disc pl-2">
-        <li>React</li>
+        <li>Angular</li>
         <li>TypeScript</li>
         <li>TailwindCSS</li>
+        <li>React</li>
       </ul>
     ),
   },
@@ -20,9 +21,10 @@ const TAB_DATA = [
     id: "backend",
     content: (
       <ul className="list-disc pl-2">
-        <li>C#</li>
-        <li>ASP.NET</li>
-        <li>Microsoft SQL Server</li>
+        <li>Java</li>
+        <li>Spring</li>
+        <li>NodeJS</li>
+        <li>MySQL</li>
       </ul>
     ),
   },
@@ -31,7 +33,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Titre Professionnel FullStack - DWWM</li>
+        <li>POE - Préparation à l'emploi - Ib Cegos</li>
+        <li>Titre Professionnel FullStack - DWWM - Wild Code School</li>
         <li>Licence AES - Toulouse</li>
       </ul>
     ),
@@ -51,14 +54,22 @@ const AboutSection = () => {
   return (
     <section className="text-white scroll-m-10" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={600} height={600} alt="about me" className="rounded-3xl"/>
+        <Image
+          src="/images/about-image.png"
+          width={600}
+          height={600}
+          alt="about me"
+          className="rounded-3xl"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4 mt-7">About Me</h2>
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with React, Typescript, TailwindCSS, C#, .NET, Microsoft SQL Server and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            working with React, Typescript, TailwindCSS, C#, .NET, Microsoft SQL
+            Server and Git. I am a quick learner and I am always looking to
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
