4c0d22e49c4380e9a65c1b1d222a95d1b8a6d2ba
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-06 16:00:11 +0100

    Updating about me section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -64,12 +64,8 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4 mt-7">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with React, Typescript, TailwindCSS, C#, .NET, Microsoft SQL
-            Server and Git. I am a quick learner and I am always looking to
-            expand my knowledge and skill set. I am a team player and I am
-            excited to work with others to create amazing applications.
+            I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience
+            working with Angular, Typescript, TailwindCSS, React in Front-End projects and with Java-Spring, NodeJS, MySQL in Back-End projects. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
